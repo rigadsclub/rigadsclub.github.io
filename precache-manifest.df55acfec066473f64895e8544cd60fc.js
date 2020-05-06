888cae5364a730708c69e5e012d1d425edb30bc9
Dmitrijs Jemeljanovs <dmitrijs@mobilly.lv>
2020-05-06 23:17:52 +0300

    Updates

---
+++ precache-manifest.df55acfec066473f64895e8544cd60fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92587493c31de8dc69ca868fa24f45c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "606c67eb2af0531d0c33",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "576763a5c284cc8330c5",
+    "url": "/static/js/2.99f4a981.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99f4a981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606c67eb2af0531d0c33",
+    "url": "/static/js/main.dd4e48a2.chunk.js"
+  },
+  {
+    "revision": "c78fa645d88fcfcb1fb1",
+    "url": "/static/js/runtime-main.68a96e7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
